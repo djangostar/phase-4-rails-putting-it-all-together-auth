b3eb2651cac21481abdfac7d3b9659258fa28251
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-10-09 15:28:51 -0700

    last commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
   
   before_action :authorize
+  
   private
 
   def authorize
