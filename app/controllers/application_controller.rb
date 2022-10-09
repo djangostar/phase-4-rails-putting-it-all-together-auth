61657cc2d1e511d5ff7f3456bd633697516bfb16
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-10-09 15:30:32 -0700

    Last commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,10 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
   
   before_action :authorize
-  
+
   private
 
   def authorize
-
     @current_user = User.find_by(id: session[:user_id])
     return render json: { errors: ["Not logged in"] }, status: :unauthorized unless @current_user
   end
